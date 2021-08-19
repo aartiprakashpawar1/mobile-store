c6d0981e8429f83c5c84d2d9ecb31308f9f8f7dc
Aarti Pawar <72916943+aartiprakashpawar1@users.noreply.github.com>
2021-08-19 13:47:44 +0530

    Add files via upload

---
+++ phone-store/src/App.js
@@ -0,0 +1,30 @@
+//import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import {Switch, Route} from 'react-router-dom'
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
+export default class App extends Component {
+  render() {
+    return   <React.Fragment>
+       <Navbar/>
+       <Switch>
+         <Route exact path="/" component={ProductList}></Route>
+         <Route path="/details" component={Details}></Route>
+         <Route path="/cart" component={Cart}></Route>
+         <Route component={Default}></Route>
+         
+       </Switch>
+      <Modal/>
+    </React.Fragment>
+ 
+  }
+}
+
+
